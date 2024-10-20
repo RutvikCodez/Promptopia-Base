58fe286f31dc2757ef59fe3006ae804a0edf36cc
RutvikCodez <rutvikdarji21@gnu.ac.in>
2024-10-20 17:03:57 +0530

    build error solved

---
+++ app/api/getAllPrompt/route.ts
@@ -0,0 +1,17 @@
+import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+  try {
+    // await connectToDB();
+    // const prompts = await Prompt.find({}).populate("creator");
+    // console.log(prompts);
+    
+    // return new NextResponse(JSON.stringify(prompts), { status: 201 });
+    return new NextResponse("hello", { status: 201 });
+  } catch (error) {
+    // console.log(error);
+    return new NextResponse("Failed to fetch prompts", { status: 500 });
+  }
+};

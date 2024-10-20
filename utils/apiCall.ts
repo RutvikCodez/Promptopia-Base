ea27396a6414b89b0691e0b4012668be0b296126
RutvikCodez <rutvikdarji21@gnu.ac.in>
2024-10-20 17:50:57 +0530

    changes are made

--- utils/apiCall.ts
+++ utils/apiCall.ts
@@ -3,7 +3,6 @@ export const apiCall = async (apiString: string) => {
     `${process.env.NEXT_PUBLIC_API_BASE_URL}/${apiString}`,
     {
       next: { revalidate: 10 },
-      cache: "no-store"
     }
   );
   const posts = await response.json();

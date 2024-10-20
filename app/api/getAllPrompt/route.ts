de46c694baeca6725c08e26a187bc87e40b680e3
RutvikCodez <rutvikdarji21@gnu.ac.in>
2024-10-20 17:09:24 +0530

    code added for getAll prompt for debugging

--- app/api/getAllPrompt/route.ts
+++ app/api/getAllPrompt/route.ts
@@ -4,14 +4,13 @@ import { NextResponse } from "next/server";
 
 export const GET = async () => {
   try {
-    // await connectToDB();
-    // const prompts = await Prompt.find({}).populate("creator");
-    // console.log(prompts);
+    await connectToDB();
+    const prompts = await Prompt.find({}).populate("creator");
+    console.log(prompts);
     
-    // return new NextResponse(JSON.stringify(prompts), { status: 201 });
-    return new NextResponse("hello", { status: 201 });
+    return new NextResponse(JSON.stringify(prompts), { status: 201 });
   } catch (error) {
-    // console.log(error);
+    console.log(error);
     return new NextResponse("Failed to fetch prompts", { status: 500 });
   }
 };

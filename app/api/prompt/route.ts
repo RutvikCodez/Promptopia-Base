88865997dced50a7c9545212a9c0feadab3847e3
RutvikCodez <rutvikdarji21@gnu.ac.in>
2024-10-20 17:01:49 +0530

    route added

--- app/api/prompt/route.ts
+++ app/api/prompt/route.ts
@@ -6,6 +6,8 @@ export const GET = async () => {
   try {
     await connectToDB();
     const prompts = await Prompt.find({}).populate("creator");
+    console.log(prompts);
+    
     return new NextResponse(JSON.stringify(prompts), { status: 201 });
   } catch (error) {
     console.log(error);

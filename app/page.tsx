a4d40a4c85eb1c5b55969be4f6742456e65318f9
RutvikCodez <rutvikdarji21@gnu.ac.in>
2024-10-20 17:56:09 +0530

    changes in root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
 import TitleDesc from "@components/reusable/TitleDesc";
 import { apiCall } from "@utils/apiCall";
 import { postType } from "@utils/types";
-import { getSessionId } from "@utils/getSessionId";
 import Feed from "@components/landigPage/Feed";
-import NotAuthorized from "@components/common/NotAuthorized";
 
 
 export default async function Home() {
-  const userID = await getSessionId();
-  if (!userID) {
-    return <NotAuthorized />;
-  }
   const posts: postType[] = await apiCall("api/prompt");
   return (
     <section className="w-full flex flex-col justify-center items-center gap-10">
--- utils/apiCall.ts
+++ utils/apiCall.ts
@@ -1,9 +1,6 @@
 export const apiCall = async (apiString: string) => {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/${apiString}`,
-    {
-      next: { revalidate: 10 },
-    }
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/${apiString}`
   );
   const posts = await response.json();
   return posts;

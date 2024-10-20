3c684f6dadd10c5803c0c84125f108d8c7d05c4c
RutvikCodez <rutvikdarji21@gnu.ac.in>
2024-10-20 17:46:36 +0530

    add cache: "no-store" in apiCall function

--- utils/apiCall.ts
+++ utils/apiCall.ts
@@ -2,7 +2,8 @@ export const apiCall = async (apiString: string) => {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_BASE_URL}/${apiString}`,
     {
-      next: { revalidate: 2 },
+      next: { revalidate: 10 },
+      cache: "no-store"
     }
   );
   const posts = await response.json();
